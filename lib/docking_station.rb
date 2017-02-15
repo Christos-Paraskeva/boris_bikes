8f03e6d756055af89d0afdca1be36ab23e6ec127
Christos <chris.paraskeva10@gmail.com>
2017-02-15 19:52:33 +0000

    added initialize method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,31 +1,67 @@
+# require_relative 'bike'
+#
+#
+# class DockingStation
+#   attr_reader :bike_instance
+#   # attr_accessor :counter
+#   $counter = 2
+#   $docked_bikes = []
+#
+#   def display_bikes
+#     puts $docked_bikes
+#   end
+#
+#   def dock_bike
+#     $docked_bikes.push($bike_instance)
+#     # add = @bike_instance
+#    # @docked_bike_list.push(add)
+#   end
+#
+#   def release_bike
+#     if ($counter > 0)
+#       $bike_instance = Bike.new
+#       $counter = $counter - 1
+#     else
+#       puts "no more bikes to release!"
+#     end
+#   end
+# end
+
+
+
 require_relative 'bike'
 
 
 class DockingStation
-  attr_reader :bike_instance
+  attr_reader :bike_instance, :docked_bikes, :counter
   # attr_accessor :counter
-  $counter = 2
-
+  def initialize
+    @counter = 2
+    @docked_bikes = []
+  end
+  
 
   def display_bikes
-    puts $bike_instance
+    puts @docked_bikes
   end
 
   def dock_bike
-    $bike_instance
+    @docked_bikes.push(@bike_instance)
     # add = @bike_instance
    # @docked_bike_list.push(add)
   end
 
   def release_bike
-    if ($counter > 0)
-      $bike_instance = Bike.new
-      $counter = $counter - 1
+    if (@counter > 0)
+      @bike_instance = Bike.new
+      @counter = @counter - 1
     else
       puts "no more bikes to release!"
     end
   end
 end
+
+
 
 
 
