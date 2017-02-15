ccb98b03075a09a8431a3b83cb6d3f05faffe62b
Christos <chris.paraskeva10@gmail.com>
2017-02-15 17:26:29 +0000

    switched to global variables

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,28 +1,28 @@
 require_relative 'bike'
 
-@counter = 2
 
 class DockingStation
   attr_reader :bike_instance
   # attr_accessor :counter
+  $counter = 2
 
 
   def display_bikes
-    puts @bike_instance
+    puts $bike_instance
   end
 
   def dock_bike
-    @bike_instance
+    $bike_instance
     # add = @bike_instance
    # @docked_bike_list.push(add)
   end
 
   def release_bike
-    if !(@counter == 0)
-      @bike_instance = Bike.new
-      @counter = @counter - 1
+    if ($counter > 0)
+      $bike_instance = Bike.new
+      $counter = $counter - 1
     else
-      puts "no more bikes to relese!"
+      puts "no more bikes to release!"
     end
   end
 end
