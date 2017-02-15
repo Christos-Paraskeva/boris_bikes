f8cc628e4d77d5570dc7e18d3eb1db2cb56dd50f
Christos <chris.paraskeva10@gmail.com>
2017-02-15 12:20:44 +0000

    completed ex 8

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,12 @@
 require 'docking_station'
 
-describe 'docking_station' do
-  it "passed" do 
-    expect(DockingStation.new).to respond_to(:release_bike)
-  end
+# describe 'docking_station' do
+#   it "passed" do
+#     expect(DockingStation.new).to respond_to(:release_bike)
+#   end
+# end
+
+describe DockingStation do
+  it { is_expected.to respond_to(:release_bike) }
 end
+  
