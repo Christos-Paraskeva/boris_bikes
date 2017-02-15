2878d8db5bd96a59c04c29cea8af0368401ce013
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-02-15 17:15:01 +0000

    Added counter

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,32 +1,39 @@
 require_relative 'bike'
+
+@counter = 2
+
 class DockingStation
   attr_reader :bike_instance
-  #@docked_bike_list = []
-  
+  # attr_accessor :counter
+
+
   def display_bikes
     puts @bike_instance
   end
-    
-  
+
   def dock_bike
     @bike_instance
     # add = @bike_instance
    # @docked_bike_list.push(add)
   end
-  
+
   def release_bike
-    @bike_instance = Bike.new
+    if !(@counter == 0)
+      @bike_instance = Bike.new
+      @counter = @counter - 1
+    else
+      puts "no more bikes to relese!"
+    end
   end
 end
 
 
 
-  
-  
+
+
    #
   # if (@y == @x)
   #   @same_bike = true
   # else
   #   @same_bike = false
   # end
-
