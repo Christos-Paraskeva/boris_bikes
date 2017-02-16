a7e06f80a56e2702a88b8ba0c2e86836cd92ae87
Christos <chris.paraskeva10@gmail.com>
2017-02-16 14:54:10 +0000

    completed up to challenge 12

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,26 +1,33 @@
+
+#
+#
+#
 # require_relative 'bike'
 #
 #
 # class DockingStation
-#   attr_reader :bike_instance
+#   attr_reader :bike_instance, :docked_bikes, :counter
 #   # attr_accessor :counter
-#   $counter = 2
-#   $docked_bikes = []
+#   def initialize
+#     @counter = 2
+#     @docked_bikes = []
+#   end
+#
 #
 #   def display_bikes
-#     puts $docked_bikes
+#     puts @docked_bikes
 #   end
 #
 #   def dock_bike
-#     $docked_bikes.push($bike_instance)
+#     @docked_bikes.push(@bike_instance)
 #     # add = @bike_instance
 #    # @docked_bike_list.push(add)
 #   end
 #
 #   def release_bike
-#     if ($counter > 0)
-#       $bike_instance = Bike.new
-#       $counter = $counter - 1
+#     if (@counter > 0)
+#       @bike_instance = Bike.new
+#       @counter = counter - 1
 #     else
 #       puts "no more bikes to release!"
 #     end
@@ -28,36 +35,23 @@
 # end
 
 
-
 require_relative 'bike'
 
 
 class DockingStation
-  attr_reader :bike_instance, :docked_bikes, :counter
-  # attr_accessor :counter
-  def initialize
-    @counter = 2
-    @docked_bikes = []
-  end
+  attr_reader :bike
   
-
   def display_bikes
-    puts @docked_bikes
+    
   end
 
-  def dock_bike
-    @docked_bikes.push(@bike_instance)
-    # add = @bike_instance
-   # @docked_bike_list.push(add)
+  def dock_bike(bike)
+    @bike = bike
   end
 
   def release_bike
-    if (@counter > 0)
-      @bike_instance = Bike.new
-      @counter = @counter - 1
-    else
-      puts "no more bikes to release!"
-    end
+    fail 'no bikes available' unless @bike
+    @bike
   end
 end
 
@@ -67,9 +61,5 @@ end
 
 
 
-   #
-  # if (@y == @x)
-  #   @same_bike = true
-  # else
-  #   @same_bike = false
-  # end
+
+
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,18 +2,31 @@
 
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
-end
-
-describe DockingStation do
+  
+  
   it "should get a bike which is working" do
     # expect from bike.working? == true
     expect(Bike.new.working?).to eq true
   end
+  
+  it "should check if bike has been docked" do
+    bike = Bike.new
+    subject.dock_bike(bike)
+    expect(subject.bike).to eq bike
+end
 end
 
-describe DockingStation do
-  it { is_expected.to respond_to(:docked_bikes) }
+describe '#release_bike' do
+  it 'should tell the user that there are no bikes available' do
+    # bike = Bike.new
+#     subject.dock_bike(bike)
+   expect { subject.release_bike }.to raise_error 'no bikes available'
 end
+end
+
+# describe DockingStation do
+#   it { is_expected.to respond_to(:docked_bikes) }
+# end
 
 # describe DockingStation do
 #   it "instance variable should be the same as the new bike created" do
